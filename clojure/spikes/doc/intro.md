1b18a234df2047db2352d93b867a488150be972b
arnabold <arnabold@gmail.com>
2013-04-30 08:56:20 +0200

    spikes

---
+++ clojure/spikes/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spikes
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/spikes/test/spikes/core_test.clj
@@ -0,0 +1,7 @@
+(ns spikes.core-test
+  (:use clojure.test
+        spikes.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
