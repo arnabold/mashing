547ee06c64bb7c9f319653f30c044d749a8eff68
arnabold <arnabold@gmail.com>
2013-09-30 13:36:16 +0200

    Terminating connection - wrong

--- scheme/racket/system-programming/serve.rkt
+++ scheme/racket/system-programming/serve.rkt
@@ -1,50 +1,39 @@
 #lang racket
 
-;; just to check if (enter! "serve.rkt") works.
-(define (go)
-  'yep-it-works)
-
-;; port-no: ip port number for client connections
 (define (serve port-no)
-  ;; define a listener to accept TCP connections. tcp-listen creates a
-  ;; listening server on the local machine at the port number
-  ;; specified by port-no. The max-allow-wait argument determines the
-  ;; maximum number of client connections that can be waiting for
-  ;; acceptance. Here is set to 5. The reuse? argument is true: it
-  ;; will create a listener even if the port is involved in a
-  ;; TIME-WAIT state.
   (define listener (tcp-listen port-no 5 #t))
-  ;; The server must loop to accept connections fro the listener
   (define (loop)
     (accept-and-handle listener)
     (loop))
-  ;; put the listener loop in its own thread and have serve to run
-  ;; immediately.
+  ;; Server Thread
   (define t (thread loop))
-  ;; Have serve return an anonymous function that can be used to
-  ;; shut down the server thread and tcp listener
   (lambda ()
     (kill-thread t)
     (tcp-close listener)))
 
-;; accept-and-handle
-;; accepts a client connection using tcp-accept, which returns two
-;; values: a stream for for input from the client and a stream of
-;; output to the client.
 (define (accept-and-handle listener)
   (define-values (in out) (tcp-accept listener))
-  ;; we can put each individual connection in its own thread
-  (thread
-   (lambda ()
-     ;; just to show the effect of different threads
-     (sleep (random 10))
-     (handle in out)
-     (close-input-port in)
-     (close-output-port out))))
+  ;; Connection Thread
+  (define t (thread
+             (lambda ()
+               (sleep (random 10)) ;; just to show the effect of different threads
+               (handle in out)
+               (close-input-port in)
+               (close-output-port out))))
+  ;; Watcher Thread
+  ;; A malicious client could connect to our web server and not send
+  ;; the HTTP header, in which case a connection thread will idle
+  ;; forever, waiting for the end of the header. To avoid this
+  ;; possibility, we’d like to implement a timeout for each connection
+  ;; thread.
+  ;; One way to implement the timeout is to create a second thread
+  ;; that waits for 10 seconds, and then kills the thread that calls
+  ;; handle. Threads are lightweight enough in Racket that this
+  ;; watcher-thread strategy works well:
+  (thread (lambda ()
+            (sleep 10)
+            (kill-thread t))))
   
-;; handle
-;; for now reads and discards the request header and the write "Hello,
-;; world!" web page as the result
 (define (handle in out)
   ;; Discard the request header (up to blank line):
   (regexp-match #rx"(\r\n|^)\r\n" in)
@@ -53,4 +42,4 @@
   (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
   (display "<html><body>Hello, world!</body></html>" out))
 
-;; Note: inside geiser use C-c C-c to stop the evaluation of (serve 8080)
+
